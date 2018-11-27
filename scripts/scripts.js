47c49b171f9fe08b1371d3067694d5ccc09e7081
Adam Wilson <adamwilson1234@gmail.com>
2018-11-26 23:59:19 -0500

    side nav hacks: added links to sections within home page. when clicked the page doesnt reload and the side nav and overlay remain in place. thus i hid them programaticallyl

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,33 @@
 
 $( document ).ready(function(){
+
   // init side nav triggered by hamburger menu
   $(".button-collapse").sideNav();
+
+  /**
+   * SIDE NAV HACKS:
+   * when user clicks side a nav link that doesn't reload
+   * the page the side nav and overlay remain, so we need to:
+   * - manually hide side nav
+   * - remove overlay
+   * - re-enable scrolling on body
+   */
+  $('.smoothScroll').on('click tap', () => {
+    $('.side-nav').hide(200);
+    $('#sidenav-overlay').remove();
+    $('body').css({
+      overflow: '',
+      width: ''
+    });
+  });
+
+  /**
+   * ANOTHER SIDE NAV HACK
+   * in case we hid side nav,
+   * show it when it is opened
+   */
+  $('.button-collapse').on('click tap', () => {
+    $('.side-nav').show();
+  });
+
 });
